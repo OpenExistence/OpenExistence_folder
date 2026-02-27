b033a0ae7293b4d8358992a215b25f002a7acf06
Clement <OpenExistence@proton.me>
2026-02-27 22:56:25 +0100

    Fix Vite config for cloudflare tunnel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000
+    port: 3000,
+    allowedHosts: ['.trycloudflare.com', 'sterling-darwin-condo-convenient.trycloudflare.com']
   }
 })
